1a65798b95c33547618a92bd4fe0ab46cf44fbae
AIM-KAmi0 <c5624012@ima.aim.aoyama.ac.jp>
2025-05-23 18:40:37 +0900

    fix: text-hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-export default function Page() {
-	return (
-		<div className='border-2 font-bold text-lg text-red'>
-			helllo
-			world
-		</div>
-	);
+export default function page() {
+	return <div className='border-2 font-bold text-lg text-red'>helllo world</div>;
 }
